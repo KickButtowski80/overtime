5e1eef9132fb0c51c285df902f484203fc210c56
kickbuttowski59 <pazpaz22@yahoo.com>
2018-01-30 23:27:02 +0000

    Implemented user connection with test and in forms as well

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,8 @@ class PostsController < ApplicationController
     
     def create
         @post = Post.new(post_params)
+        # byebug
+        @post.user_id = current_user.id
         if @post.save        
             redirect_to @post, notice: 'Your post was created successfully'
         else
@@ -23,7 +25,7 @@ class PostsController < ApplicationController
     
     private
         def post_params
-            params.require(:post).permit(:data, :rationale)
+            params.require(:post).permit(:date, :rationale)
         end
         
         def set_post
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  date       :date
+#  rationale  :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer
+#
+
 class Post < ApplicationRecord
+    
+    belongs_to :user, optional: true
+    
     validates_presence_of :date, :rationale
 end
