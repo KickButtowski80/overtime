d44638913011685936b58c3a5798baafe56da961
kickbuttowski59 <pazpaz22@yahoo.com>
2018-02-09 19:09:37 +0000

    Implemented ability to block users after post marked complete

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,9 +1,22 @@
 class PostPolicy < ApplicationPolicy
-    
+ #record is a post in our case   
     def update?
-        #record is a post in our case
-        record.user_id == user.id  || admin_types.include?(user.type)
+        return true if record.approved? && admin? 
+        return true if user_or_admin && !post_approved?
         
     end
     
+    private
+        def user_or_admin
+            record.user_id == user.id  ||admin?
+        end
+        
+        def admin?
+            admin_types.include?(user.type)
+        end
+        
+        def post_approved?
+            record.approved?
+        end
+        
 end
