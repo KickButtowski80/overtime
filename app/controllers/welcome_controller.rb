5e612f087d364f7d585db06996b81f3188115c5f
kickbuttowski59 <pazpaz22@yahoo.com>
2018-02-26 12:06:24 -0800

    Added view elements for aduit items that are pending

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @pending_approvals = Post.where(status: 'submitted')
-    @recent_audit_items = Post.where(status: 'approved')
+    @recent_audit_items = AuditLog.last(10)
   end
 end
