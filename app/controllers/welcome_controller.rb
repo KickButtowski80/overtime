a6e479bea4845542f239b3c1b6b0d8e3aa89e55c
kickbuttowski59 <pazpaz22@yahoo.com>
2018-02-26 11:37:28 -0800

    Added tasks to Admin descriptions for more ease such as going to pending items or approves items

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @pending_approvals = Post.where(status: 'submitted')
+    @recent_audit_items = Post.where(status: 'approved')
   end
 end
