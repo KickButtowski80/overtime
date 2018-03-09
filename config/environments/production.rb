5c29943d55f878646cfc7b5f1634a44b052ac3d0
kickbuttowski59 <pazpaz22@yahoo.com>
2018-03-08 19:45:56 -0800

    updated value of action mailer for proudction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,5 +94,5 @@ Rails.application.configure do
   config.serve_static_files = true 
   config.assets.digest = true
   
-   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { :host => 'overtime-kick-buttoskw.herokuapp.com' }
 end
