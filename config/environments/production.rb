f770d78082e94a44d2b92bf9e30be3e6fb5fa8c2
kickbuttowski59 <pazpaz22@yahoo.com>
2018-04-08 19:23:58 -0700

    changed seed file and pro rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,10 +95,5 @@ Rails.application.configure do
   config.assets.digest = true
    
   config.action_mailer.default_url_options = {host: 'overtime-kick-buttoskwi.herokuapp.com/' , protocol: 'https://'}
-  #  tested not working 
-   config.action_mailer.delivery_method = :mailgun
-  # config.action_mailer.mailgun_settings = {
-  # # api_key: 'ENV["mailgun_secret_api_key"]',
-  # domain: 'overtime-kick-buttoskwi.herokuapp.com',
-  # }
+ 
 end
